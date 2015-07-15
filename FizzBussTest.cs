ca41ac1e7c864c8c9e430de92972b0a391cb58e0
Travis K <traviskean04@live.com>
2015-07-15 16:05:16 -0400

    FizzBuzz Test Project

---
+++ FizzBussTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace First_TDD_Demo.Test
+{
+    public class FizzBussTest
+    {
+        [Fact]
+        public void ReturnedNumberAsString()
+        {
+            var ourTest = new First_TDD_Demo.FizzBuzzLogic();
+            string answer = ourTest.DoTheThing(1);
+
+            Assert.Equal("1", answer);
+        }
+    }
+}
